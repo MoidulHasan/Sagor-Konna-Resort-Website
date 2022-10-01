0cfc971f0f5430e88d89711e7f095cba671aee15
najim473 <najim7304@gmail.com>
2022-10-01 17:28:43 +0600

    added img_container

--- components/room/RoomCard.js
+++ components/room/RoomCard.js
@@ -1,17 +1,18 @@
-import Image from 'next/image';
 import { Card } from 'primereact/card';
+import { Button } from 'primereact/button';
 import roomCardStyles from '../../styles/room.module.css'
 const RoomCard = ({ roomData }) => {
     return (
         <div>
             <div className="flex flex align-items-center justify-content-center flex-wrap overflow-hidden">
-                {roomData.map((card) => (
+                {roomData.slice(0, 1).map((card) => (
                     <Card className={` m-2 ${roomCardStyles.room_card}`} key={card.id} >
-                        <div className={`${roomCardStyles.img_box}`}>
-                            <div style={{ backgroundImage: `url(${card.img})` }} className={`relative ${roomCardStyles.card_img}`}>
-                                <p className='absolute top-0 text-xl'>{card.status}</p>
+                        <div className={roomCardStyles.img_container}>
+                            <div className={`${roomCardStyles.img_box}`}>
+                                <div style={{ backgroundImage: `url(${card.img})` }} className={`relative ${roomCardStyles.card_img}`}>
+                                </div>
                             </div>
-
+                            <span className={`${roomCardStyles.status}`}>{card.status}</span>
                         </div>
                     </Card>
                 ))}
--- pages/api/roomData.js
+++ pages/api/roomData.js
@@ -5,6 +5,7 @@ const data = [
         "img": "/image/Room/room01.jpg",
         "status": "Popular",
         "type": "Guest",
+        "time": "night",
         "description": "      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente qui esse pariatur fugit animi dolorum...",
         "price": "160.0"
 
@@ -14,6 +15,7 @@ const data = [
         "title": "Nature view",
         "img": "/image/Room/room06.jpg",
         "type": "Guest",
+        "time": "night",
         "description": "      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente qui esse pariatur fugit animi dolorum...",
         "price": "120.0"
     },
@@ -22,6 +24,7 @@ const data = [
         "title": "See View",
         "img": "/image/Room/room07.jpg",
         "type": "Guest",
+        "time": "night",
         "status": "Popular",
         "description": "      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente qui esse pariatur fugit animi dolorum...",
         "price": "150.0"
