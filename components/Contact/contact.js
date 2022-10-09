8d7e1ca1199e1b548d28ef38415134f0111273d9
najim473 <najim7304@gmail.com>
2022-10-09 21:33:33 +0600

    added contact in skr-contact

---
+++ components/Contact/contact.js
@@ -0,0 +1,13 @@
+
+const Contact = () => {
+    return (
+        <div>
+            <h1>Get in Touch</h1>
+            <p>
+                Consectetur adipisicing elit. Nihil, illum voluptate eveniet ex fugit ea delectus, sed voluptatem. Laborum accusantium libero commodi id officiis itaque esse adipisci, necessitatibus asperiores, illo odio.
+            </p>
+        </div>
+    )
+}
+
+export default Contact
