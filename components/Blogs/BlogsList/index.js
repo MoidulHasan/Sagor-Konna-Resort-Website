1435eb2a2dbe484866e7104f3a94853850ddb0e7
hasan75 <hasanjab14@gmail.com>
2022-10-18 20:17:22 +0600

    blogs page initiated for #30

---
+++ components/Blogs/BlogsList/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const BlogList = ({ blogs }) => {
+  return <div>{blogs.length}</div>;
+};
+
+export default BlogList;
--- pages/blogs/index.js
+++ pages/blogs/index.js
@@ -1,22 +1,23 @@
-import Blog from '../../components/Blogs/Blogs'
-const blogs = ({ blog }) => {
-    return (
-        <div>
-            <Blog blog={blog} />
-        </div>
-    )
-}
+import BlogHeader from '../../components/Blogs/BlogHeader';
+import BlogList from '../../components/Blogs/BlogsList';
+const Blogs = ({ blogs }) => {
+  console.log(blogs);
+  return (
+    <div>
+      <BlogHeader />
+      <BlogList blogs={blogs} />
+    </div>
+  );
+};
 
-export default blogs
+export default Blogs;
 export async function getStaticProps() {
-    // const roomdata = await import('./api/roomData.json');
-    // return { props: { roomdata: roomdata.default } };
-    const res = await fetch('http://localhost:3000/api/blogData', {
-        accept: 'application/json',
-    });
-    // console.log((res));
-    const blog = await res.json();
-    return {
-        props: { blog: blog },
-    };
+  const res = await fetch('http://localhost:3000/api/blogData', {
+    accept: 'application/json',
+  });
+
+  const blogs = await res.json();
+  return {
+    props: { blogs: blogs },
+  };
 }
